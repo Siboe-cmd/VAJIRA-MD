9efe570a99ce23a6930681475dd524e4120a0156
Sibloe <80690203+Siboe-cmd@users.noreply.github.com>
2024-08-03 17:48:24 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '263717087726' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=jJdGAAbR#f2DMnQGFiFpkxXJmfcmu84DYc7DI4FwprLpjFx9GZYI' ,
+SESSION_ID: process.env.SESSION_ID || '1VAJIRA-MD=uQ8GjTAb#UAKvsVLu4DfV8WjxYhayyPsTdsdolbqT7fqFXMbZU-0' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
